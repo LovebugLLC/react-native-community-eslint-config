db755a3e188bbca053b35e8bfcdca4d6d63713be
AP Fritts <2000021+apfritts@users.noreply.github.com>
2022-04-01 16:18:19 -0700

    Switch parser

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports = {
   overrides: [
     {
       files: ['*.js'],
-      parser: 'babel-eslint',
+      parser: '@babel/eslint-parser',
       plugins: ['flowtype'],
       rules: {
         // Flow Plugin
